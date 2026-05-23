795fc36eb90972246551b3c4b919ee22df725c32
abhishek-150104 <abhishekkumarshrivastav61@gmail.com>
2026-05-23 10:20:05 +0530

    Feat: System Prompts

--- chat.py
+++ chat.py
@@ -1,10 +1,17 @@
 from client import client
 from config import model, max_tokens
 
-def chat(messages):
-    message = client.messages.create(
-        model=model,
-        max_tokens=max_tokens,
-        messages=messages
-    )
+def chat(messages,system=None):
+
+    params = {
+        "model": model,
+        "max_tokens": max_tokens,
+        "messages": messages
+    }
+
+    if system:
+        params["system"] = system
+
+    message = client.messages.create(**params)
+    
     return message.content[0].text
--- main.py
+++ main.py
@@ -9,9 +9,17 @@ while True:
     user_input = input("> ")
     print(">",user_input)
 
+    # system = input("> ")
+
+    system = """
+    You are a patient math tutor.
+    Do not directly answer a student's questions.
+    Guide them to a solution step by step
+    """
+
     add_user_message(messages, user_input)
 
-    reply = chat(messages)
+    reply = chat(messages,system)
 
     add_assistant_message(messages, reply)
 
